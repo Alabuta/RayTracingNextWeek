6fa7afbdb2cd1d8bc153840a6847d316a5ff0b20
Almaz Kinziabulatov <auqolaq@gmail.com>
2019-05-19 14:59:36 +0300

    Simple framebuffer abstraction.

--- Raytracer/src/gfx/context.hxx
+++ Raytracer/src/gfx/context.hxx
@@ -36,4 +36,44 @@ void init_context(platform::window const &window)
     if (auto result = glewInit(); result != GLEW_OK)
         throw std::runtime_error("failed to init GLEW"s);
 }
+struct framebuffer final {
+    std::array<std::int32_t, 2> size{0, 0};
+
+    std::uint32_t handle{0u};
+    std::uint32_t attachment_handle{0u};
+
+    framebuffer(std::int32_t width, std::int32_t height) : size{std::array{width, height}}
+    {
+        glCreateFramebuffers(1, &handle);
+        glObjectLabel(GL_FRAMEBUFFER, handle, -1, "[framebuffer object]");
+
+        glCreateTextures(GL_TEXTURE_2D, 1, &attachment_handle);
+        glObjectLabel(GL_TEXTURE, attachment_handle, -1, "[texture object]");
+
+        glTextureParameteri(attachment_handle, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTextureParameteri(attachment_handle, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTextureParameteri(attachment_handle, GL_TEXTURE_MAX_LEVEL, 0);
+        glTextureParameteri(attachment_handle, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTextureParameteri(attachment_handle, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+        glTextureStorage2D(attachment_handle, 1, GL_SRGB8_ALPHA8, width, height);
+
+        glNamedFramebufferTexture(handle, GL_COLOR_ATTACHMENT0, attachment_handle, 0);
+
+        std::array<std::uint32_t, 1> constexpr drawBuffers{GL_COLOR_ATTACHMENT0};
+        glNamedFramebufferDrawBuffers(handle, static_cast<std::int32_t>(std::size(drawBuffers)), std::data(drawBuffers));
+
+        if (auto result = glCheckNamedFramebufferStatus(handle, GL_FRAMEBUFFER); result != GL_FRAMEBUFFER_COMPLETE)
+            throw std::runtime_error("framebuffer: "s + std::to_string(result));
+
+        if (auto result = glGetError(); result != GL_NO_ERROR)
+            throw std::runtime_error("OpenGL error: "s + std::to_string(result));
+    }
+
+    ~framebuffer()
+    {
+        glDeleteTextures(1, std::data(std::array{attachment_handle}));
+        glDeleteFramebuffers(1, std::data(std::array{handle}));
+    }
+};
 }
--- Raytracer/src/main.cxx
+++ Raytracer/src/main.cxx
@@ -4,6 +4,8 @@
 
 
 namespace app {
+auto clear_color = std::array{0.f, .254901f, .6f, 1.f};
+
 struct properties final {
 
     std::array<std::int32_t, 2> window_size{800, 600};
@@ -48,18 +50,31 @@ int main()
     auto app_window_event_handler = std::make_shared<app::window_event_handler>(app_properties);
     window.connect_handler(app_window_event_handler);
 
-    glClearColor(.5f, .5f, .5f, 1.f);
 
-    window.update([&app_properties] (auto &&window)
+    gfx::framebuffer framebuffer{width, height};
+
+    if (auto result = glGetError(); result != GL_NO_ERROR)
+        throw std::runtime_error("OpenGL error: "s + std::to_string(result));
+
+    window.update([&] (auto &&window)
     {
         glfwPollEvents();
 
         auto [width, height] = app_properties.window_size;
 
+        glBindFramebuffer(GL_DRAW_FRAMEBUFFER, framebuffer.handle);
+
+        glClearNamedFramebufferfv(framebuffer.handle, GL_COLOR, 0, std::data(app::clear_color));
+        glClearNamedFramebufferfi(framebuffer.handle, GL_DEPTH_STENCIL, 0, 1.f, 1);
+
         glViewport(0, 0, width, height);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glBlitNamedFramebuffer(framebuffer.handle, 0, 0, 0, width, height, 0, 0, width, height, GL_COLOR_BUFFER_BIT, GL_NEAREST);
 
         glfwSwapBuffers(window.handle());
+
+        if (auto result = glGetError(); result != GL_NO_ERROR)
+            throw std::runtime_error("OpenGL error: "s + std::to_string(result));
     });
 
     glfwTerminate();
