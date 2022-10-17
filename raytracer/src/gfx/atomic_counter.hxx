5d2d9fa11a04c9ec31b187e1979ce2e6a7743f10
Almaz K <auqolaq@gmail.com>
2022-10-17 21:24:45 +0300

    Minor changes.

--- raytracer/src/gfx/atomic_counter.hxx
+++ raytracer/src/gfx/atomic_counter.hxx
@@ -6,44 +6,46 @@
 #include "gfx/context.hxx"
 
 
-namespace gfx {
-template<class T>
-struct atomic_counter_buffer final {
-    using type = T;
-
-    std::uint32_t handle{0};
-    std::uint32_t binding_index{0};
-};
-
-template<class T>
-gfx::atomic_counter_buffer<T> create_atomic_counter_buffer(std::uint32_t binding_index)
+namespace gfx
 {
-    std::uint32_t handle;
+    template<class T>
+    struct atomic_counter_buffer final {
+        using type = T;
 
-    glCreateBuffers(1, &handle);
-    glObjectLabel(GL_BUFFER, handle, -1, "[BO]");
+        std::uint32_t handle{ 0 };
+        std::uint32_t binding_index{ 0 };
+    };
 
-    glNamedBufferStorage(handle, sizeof(T), nullptr, GL_DYNAMIC_STORAGE_BIT);
-    glBindBufferBase(GL_ATOMIC_COUNTER_BUFFER, binding_index, handle);
+    template<class T>
+    gfx::atomic_counter_buffer<T> create_atomic_counter_buffer(std::uint32_t binding_index)
+    {
+        std::uint32_t handle;
 
-    return gfx::atomic_counter_buffer<T>{handle, binding_index};
-}
+        glCreateBuffers(1, &handle);
+        glObjectLabel(GL_BUFFER, handle, -1, "[BO]");
 
-template<class T>
-void update_atomic_counter_buffer(gfx::atomic_counter_buffer<T> const &buffer, T data)
-{
-    auto internal_format = 0u;
-    auto data_format = 0u;
-    auto data_type = 0u;
+        glNamedBufferStorage(handle, sizeof(T), nullptr, GL_DYNAMIC_STORAGE_BIT);
+        glBindBufferBase(GL_ATOMIC_COUNTER_BUFFER, binding_index, handle);
 
-    if constexpr (std::is_same_v<T, std::uint32_t>) {
-        internal_format = GL_R32UI;
-        data_format = GL_RED;
-        data_type = GL_UNSIGNED_INT;
+        return gfx::atomic_counter_buffer<T>{ handle, binding_index };
     }
 
-    else throw std::runtime_error("unsupported type"s);
+    template<class T>
+    void update_atomic_counter_buffer(gfx::atomic_counter_buffer<T> const &buffer, T data)
+    {
+        auto internal_format = 0u;
+        auto data_format = 0u;
+        auto data_type = 0u;
 
-    glClearNamedBufferSubData(buffer.handle, internal_format, 0, sizeof(T), data_format, data_type , &data);
-}
+        if constexpr (std::is_same_v<T, std::uint32_t>) {
+            internal_format = GL_R32UI;
+            data_format = GL_RED;
+            data_type = GL_UNSIGNED_INT;
+        }
+
+        else
+            throw std::runtime_error("unsupported type"s);
+
+        glClearNamedBufferSubData(buffer.handle, internal_format, 0, sizeof(T), data_format, data_type, &data);
+    }
 }
--- raytracer/src/gfx/shader_storage.hxx
+++ raytracer/src/gfx/shader_storage.hxx
@@ -6,53 +6,55 @@
 #include "gfx/context.hxx"
 
 
-namespace gfx {
-template<class T>
-struct shader_storage_buffer final {
-    using type = T;
-
-    std::uint32_t handle{0};
-    std::uint32_t binding_index{0};
-    std::uint32_t length{0};
-};
-
-template<class T>
-gfx::shader_storage_buffer<T> create_shader_storage_buffer(std::uint32_t binding_index, std::uint32_t length, T const *data)
+namespace gfx
 {
-    std::uint32_t handle;
+    template<class T>
+    struct shader_storage_buffer final {
+        using type = T;
 
-    glCreateBuffers(1, &handle);
-    glObjectLabel(GL_BUFFER, handle, -1, "[BO]");
+        std::uint32_t handle{ 0 };
+        std::uint32_t binding_index{ 0 };
+        std::uint32_t length{ 0 };
+    };
 
-    auto size_in_bytes = static_cast<std::ptrdiff_t>(sizeof(T) * length);
+    template<class T>
+    gfx::shader_storage_buffer<T>
+    create_shader_storage_buffer(std::uint32_t binding_index, std::uint32_t length, T const *data)
+    {
+        std::uint32_t handle;
 
-    glNamedBufferStorage(handle, size_in_bytes, data, 0);
-    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, binding_index, handle);
+        glCreateBuffers(1, &handle);
+        glObjectLabel(GL_BUFFER, handle, -1, "[BO]");
 
-    return gfx::shader_storage_buffer<T>{handle, binding_index, length};
-}
+        auto size_in_bytes = static_cast<std::ptrdiff_t>(sizeof(T) * length);
 
-template<class T>
-gfx::shader_storage_buffer<T> create_shader_storage_buffer(std::uint32_t binding_index, std::uint32_t length)
-{
-    std::uint32_t handle;
+        glNamedBufferStorage(handle, size_in_bytes, data, 0);
+        glBindBufferBase(GL_SHADER_STORAGE_BUFFER, binding_index, handle);
 
-    glCreateBuffers(1, &handle);
-    glObjectLabel(GL_BUFFER, handle, -1, "[BO]");
+        return gfx::shader_storage_buffer<T>{ handle, binding_index, length };
+    }
 
-    auto size_in_bytes = static_cast<std::ptrdiff_t>(sizeof(T) * length);
+    template<class T>
+    gfx::shader_storage_buffer<T> create_shader_storage_buffer(std::uint32_t binding_index, std::uint32_t length)
+    {
+        std::uint32_t handle;
 
-    glNamedBufferStorage(handle, size_in_bytes, nullptr, GL_DYNAMIC_STORAGE_BIT);
-    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, binding_index, handle);
+        glCreateBuffers(1, &handle);
+        glObjectLabel(GL_BUFFER, handle, -1, "[BO]");
 
-    return gfx::shader_storage_buffer<T>{handle, binding_index, length};
-}
+        auto size_in_bytes = static_cast<std::ptrdiff_t>(sizeof(T) * length);
 
-template<class T>
-void update_shader_storage_buffer(gfx::shader_storage_buffer<T> const &buffer, T const *data)
-{
-    auto size_in_bytes = static_cast<std::ptrdiff_t>(sizeof(T) * buffer.length);
+        glNamedBufferStorage(handle, size_in_bytes, nullptr, GL_DYNAMIC_STORAGE_BIT);
+        glBindBufferBase(GL_SHADER_STORAGE_BUFFER, binding_index, handle);
 
-    glNamedBufferSubData(buffer.handle, 0, size_in_bytes, data);
-}
+        return gfx::shader_storage_buffer<T>{ handle, binding_index, length };
+    }
+
+    template<class T>
+    void update_shader_storage_buffer(gfx::shader_storage_buffer<T> const &buffer, T const *data)
+    {
+        auto size_in_bytes = static_cast<std::ptrdiff_t>(sizeof(T) * buffer.length);
+
+        glNamedBufferSubData(buffer.handle, 0, size_in_bytes, data);
+    }
 }
